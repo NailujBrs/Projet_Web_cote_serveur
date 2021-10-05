2716eb446d5923d416d0bad1578fb7edf364494f
NailujBrs <borsjulian@gmail.com>
2021-10-05 14:43:52 +0200

    Ajout des valeurs lignes par lignes dans un objet these Save non fonctionnel

--- script/import.php
+++ script/import.php
@@ -10,7 +10,7 @@ while (!feof($file)) {
     $line = fgets($file);
     $sep = explode(";", $line);
     $these = new theses($sep[0], $sep[1], $sep[2], $sep[3], $sep[5], $sep[6], $sep[7], $sep[8], $sep[9],$dump->changeDate($sep[10]),$dump->changeDate($sep[11]), $sep[12], $sep[13], $sep[14],$dump->changeDate($sep[15]),$dump->changeDate($sep[16]));
-
+    $these->save();
 }
 fclose($file);
 
