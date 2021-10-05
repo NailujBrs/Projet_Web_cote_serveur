4a749b6c63964f47fd0193758b435a7db0ab27c7
NailujBrs <borsjulian@gmail.com>
2021-10-05 16:17:59 +0200

    ajout de save dans la DB

--- script/import.php
+++ script/import.php
@@ -6,11 +6,13 @@ $dump = new dump();
 
 $file = fopen("../files/extraction.csv", "r") or die ("Impossible to open the file");
 fgets($file);
+
 while (!feof($file)) {
     $line = fgets($file);
     $sep = explode(";", $line);
     $these = new theses($sep[0], $sep[1], $sep[2], $sep[3], $sep[5], $sep[6], $sep[7], $sep[8], $sep[9],$dump->changeDate($sep[10]),$dump->changeDate($sep[11]), $sep[12], $sep[13], $sep[14],$dump->changeDate($sep[15]),$dump->changeDate($sep[16]));
     $these->save();
+
 }
 fclose($file);
 
