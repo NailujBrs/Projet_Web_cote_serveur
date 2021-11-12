68ffaacbb1673387be9ca12cc388d36f43ff18be
NailujBrs <borsjulian@gmail.com>
2021-11-12 16:41:02 +0100

    affichage recherche

--- script/import.php
+++ script/import.php
@@ -3,7 +3,8 @@ include("../class/theses.php");
 include("../class/dump.php");
 
 $dump = new dump();
-$file = fopen("extract5.csv", "r") or die ("Impossible to open the file");
+
+$file = fopen("../files/extraction.csv", "r") or die ("Impossible to open the file");
 fgets($file);
 
 while (!feof($file)) {
@@ -11,6 +12,8 @@ while (!feof($file)) {
     $sep = explode(";", $line);
     $these = new theses($sep[0], $sep[1], $sep[2], $sep[3], $sep[5], $sep[6], $sep[7], $sep[8], $sep[9],$dump->changeDate($sep[10]),$dump->changeDate($sep[11]), $sep[12], $sep[13], $sep[14],$dump->changeDate($sep[15]),$dump->changeDate($sep[16]));
     $these->save();
+
 }
 fclose($file);
+
 ?>
