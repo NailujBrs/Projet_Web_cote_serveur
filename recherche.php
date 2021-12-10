ffca4363239a6781b02ad0bc6818e8eae4823641
NailujBrs <borsjulian@gmail.com>
2021-12-10 13:52:25 +0100

    ajout case de scroll pour les recherches

--- recherche.php
+++ recherche.php
@@ -5,14 +5,13 @@ echo "<!DOCTYPE html>
     <meta charset=\"UTF-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0\"/>
     <link rel=\"stylesheet\" href=\"css/recherche_search.css\">
+    <script src=\"https://code.highcharts.com/highcharts.src.js\"></script>
     <title>Theses</title>
 </head>
     <div>
         <a href='index.html' title='Page accueil'>
             <img src=\"img/newLogo.png\" class=\"logo\"/>
         </a>
-    </div>
-    <div>
         <form action=\"recherche.php\" name=\"search\" method=\"post\" class=\"search\">
             <div>
                 <input type=\"text\" name=\"search\"  class=\"search_bar\" placeholder=\"Recherche...\">
@@ -34,11 +33,11 @@ if (isset($_POST["search"]) AND !empty($_POST['search'])) {
 
     $request->execute();
 
-    echo "<ul>";
+    echo "<div class='resultat'><ul>";
     while ($line = $request->fetch()) {
         echo "<li class='titre'><a href='https://theses.fr/".$line['id_these']."' target='_blank' class='link'>".$line['title']."<a></li><br>";
     }
-    echo "</ul>";
+    echo "</ul></div>";
 }
 
 else if (empty($_POST['search'])) {
