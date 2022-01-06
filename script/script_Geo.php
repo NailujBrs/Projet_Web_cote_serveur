3bce294a9f9a9a7b147fa79b51f7e83e590c862e
NailujBrs <borsjulian@gmail.com>
2022-01-06 09:23:56 +0100

    Import BDD Map

---
+++ script/script_Geo.php
@@ -0,0 +1,20 @@
+<?php
+include("../class/connexion.php");
+ini_set('max_execution_time','0');
+header("Content-Type: application/json; charset=utf-8");
+
+$cnx = new connexion();
+$db = $cnx->getCnx();
+
+$file = file_get_contents("../files/info_map.json");
+$array = json_decode($file,true);
+
+foreach ($array as $donnee) {
+    $sql = "INSERT INTO Map VALUES (:id,:long,:lat);";
+    $requete = $db->prepare($sql);
+    $requete->bindParam('id',$donnee['fields']["identifiant_idref"],PDO::PARAM_STR,40);
+    $requete->bindParam('long',$donnee['fields']['coordonnees'][0],PDO::PARAM_STR,20);
+    $requete->bindParam('lat',$donnee['fields']['coordonnees'][1],PDO::PARAM_STR,20);
+    $requete->execute();
+}
+?>
